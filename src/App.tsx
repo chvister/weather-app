9215da4c8d1e81c10ae0fb20e0c6a4fc91e04fb2
Martin Stolar <martin.stolars@gmail.com>
2023-10-17 14:23:59 +0200

    fix added .env

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Weather from "./components/Weather";
 import { Provider } from "react-redux";
--- src/services/weatherAPI.ts
+++ src/services/weatherAPI.ts
@@ -1,7 +1,6 @@
 import { WeatherResponse } from "../types/WeatherResponse";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://api.openweathermap.org/data/2.5/weather",
@@ -10,7 +9,7 @@ export const api = createApi({
   endpoints: (build) => ({
     getWeatherByName: build.query<WeatherResponse, string>({
       query: (name) =>
-        `?q=${name}&units=metric&appid=ef3e0a3db891971abc90401d074e22fa`,
+        `?q=${name}&units=metric&appid=${process.env.REACT_APP_API_KEY}`,
     }),
   }),
 });
